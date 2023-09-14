264c01587fac2738647cc47245ff7b97fcc50b6d
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-09-14 17:36:46 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 import App from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <BrowserRouter basename="/your_repo_name">
+          <App />
+        </BrowserRouter>
       </Provider>
     </React.StrictMode>
 );
