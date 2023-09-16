10f21e5abdb86b528441c8f1d2eef8e3bd77773c
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-09-16 20:48:47 +0200

    commit

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,17 +2,18 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "redux/auth/useAuth";
 // import styles from '../App.module.css';
 import styles from './Navigation.module.css';
+import { CONTACTS_ROUTE, HOME_ROUTE } from 'config/routes';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <nav className={styles.nav}>
-      <NavLink className={styles.link} to="{HOME_ROUTE}">
+      <NavLink className={styles.link} to={HOME_ROUTE}>
         Home
       </NavLink>
       {isLoggedIn && (
-        <NavLink className={styles.link} to="{CONTACTS_ROUTE}">
+        <NavLink className={styles.link} to={CONTACTS_ROUTE}>
           Phonebook
         </NavLink>
       )}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 
+const styles = {
+  container: {
+    // minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    minHeight: 'calc(100vh - 50px)',
+    fontWeight: 500,
+    fontSize: 48,
+    textAlign: 'center',
+  },
+  paragraph: {
+    fontWeight: 300,
+    fontSize: 24,
+    textAlign: 'center',
+  },
+};
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Phonebook App</h1>
-      <p>This is the home page of the Phonebook App. Please log in to access your contacts.</p>
+    <div style={styles.container}>
+      <h1 style={styles.title}>Welcome to Phonebook App</h1>
+      <p style={styles.paragraph}>This is the home page of the Phonebook App. Please log in to access your contacts.</p>
     </div>
   );
 };
 
 export default HomePage;
+
+
+// const HomePage = () => {
+//   return (
+//     <div style={styles.container}>
+//       <h1>Welcome to Phonebook App</h1>
+//       <p >This is the home page of the Phonebook App. Please log in to access your contacts.</p>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
