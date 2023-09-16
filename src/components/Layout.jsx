e1d7d8625645c5253af66c14566f51a7ebef5740
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-09-16 18:20:53 +0200

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import { Suspense } from 'react';
 import styles from './App.module.css';
 import { Navigation } from './Navigation/Navigation';
 import UserMenu from './UserMenu/UserMenu';
@@ -20,13 +18,26 @@ const AppBar = () => {
 
 export const Layout = () => {
   return (
-    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+    <>
       <AppBar />
-      <Suspense fallback={null}>
         <Outlet />
-      </Suspense>
-      <Toaster position="top-right" reverseOrder={false} />
-    </div>
+    </>
   );
 };
+
+
+// import { Toaster } from 'react-hot-toast';
+// import { Suspense } from 'react';
+
+// export const Layout = () => {
+//   return (
+//     <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+//       <AppBar />
+//       <Suspense fallback={null}>
+//         <Outlet />
+//       </Suspense>
+//       <Toaster position="top-right" reverseOrder={false} />
+//     </div>
+//   );
+// };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -14,6 +14,7 @@ import {
   PERSIST,
   PURGE,
 } from 'redux-persist';
+
 const persistConfig = {
   key: 'contacts',
   storage,
