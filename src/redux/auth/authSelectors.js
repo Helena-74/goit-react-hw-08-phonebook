488c597a63e5400fcaabb79ff5a5348a8b387930
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-09-16 15:23:29 +0200

    commit

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -3,8 +3,8 @@ import { createAction, createSelector } from "@reduxjs/toolkit";
 
 export const selectIsAuthenticated = (state) => state.auth.isAuthenticated;
 export const selectUser = (state) => state.auth.user;
-export const selectIsLoggedIn = (state )=> state.auth.isLoggedIn;
-export const selectIsRefreshing = (state )=> state.auth.isRefreshing;
+export const selectIsLoggedIn = (state)=> state.auth.isLoggedIn;
+export const selectIsRefreshing = (state)=> state.auth.isRefreshing;
 
 export const selectUserEmail = createSelector(
   [selectUser],
